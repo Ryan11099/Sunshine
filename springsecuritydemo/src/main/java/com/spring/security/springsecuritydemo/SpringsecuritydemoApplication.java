89db7259e2ac5d4aa02b75ccdc0e6e69bf727547
root <1109936882@qq.com>
2020-07-08 10:21:59 +0800

    添加依赖

--- springsecuritydemo/src/main/java/com/spring/security/springsecuritydemo/SpringsecuritydemoApplication.java
+++ springsecuritydemo/src/main/java/com/spring/security/springsecuritydemo/SpringsecuritydemoApplication.java
@@ -1,13 +1,17 @@
 package com.spring.security.springsecuritydemo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class SpringsecuritydemoApplication {
 
     public static void main(String[] args) {
+        log.info("项目启动。。。");
         SpringApplication.run(SpringsecuritydemoApplication.class, args);
+        log.info("项目启动完成。。。");
     }
 
 }
